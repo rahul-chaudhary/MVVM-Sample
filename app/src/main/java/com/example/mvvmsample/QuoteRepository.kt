8fbf29fcdbf40691099f00754009b6428c335ff2
unknown <bossrc64@outlook.com>
2024-03-30 11:22:39 +0530

    Add: Functions in QuoteReposity

--- app/src/main/java/com/example/mvvmsample/QuoteRepository.kt
+++ app/src/main/java/com/example/mvvmsample/QuoteRepository.kt
@@ -1,4 +1,14 @@
 package com.example.mvvmsample
 
-class QuoteRepository {
+import androidx.lifecycle.LiveData
+
+class QuoteRepository(private val quoteDao: QuoteDao) {
+
+    fun getQuotes(): LiveData<List<Quote>> {
+        return quoteDao.getQuotes()
+    }
+
+    suspend fun insertQuote(quote: Quote) {
+        quoteDao.insertQuote(quote)
+    }
 }
