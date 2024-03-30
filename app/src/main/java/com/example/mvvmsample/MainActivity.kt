c61f5f06010d4b84b945d65cb59405d516ec04a9
unknown <bossrc64@outlook.com>
2024-03-30 12:26:36 +0530

    Add: Change in UI and MainActivity

--- app/src/main/java/com/example/mvvmsample/MainActivity.kt
+++ app/src/main/java/com/example/mvvmsample/MainActivity.kt
@@ -5,17 +5,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        binding.btnAddQuote.setOnClickListener {
+        val dao = QuoteDatabase.getDatabase(applicationContext).quoteDao()
+        val repository = QuoteRepository(dao)
 
+        mainViewModel =
+            ViewModelProvider(this, MainViewModelFactory(repository))[MainViewModel::class.java]
+
+        mainViewModel.getQuotes().observe(
+            this
+        ) {
+        binding.quotes = it.toString()
+        }
+
+        binding.btnAddQuote.setOnClickListener {
+        val quote = Quote(0, "You know me", "GOD")
         }
 
     }
