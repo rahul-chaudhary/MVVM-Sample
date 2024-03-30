ec41f26a7cd0bad12605db0e927b821f39b18340
unknown <bossrc64@outlook.com>
2024-03-30 12:18:37 +0530

    Add: MainAcitivity MainViewModelFactory

--- app/src/main/java/com/example/mvvmsample/MainViewModelFactory.kt
+++ app/src/main/java/com/example/mvvmsample/MainViewModelFactory.kt
@@ -1,4 +1,12 @@
 package com.example.mvvmsample
 
-class MainViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MainViewModelFactory(private val quoteRepository: QuoteRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainViewModel(quoteRepository) as T
+    }
+
 }
